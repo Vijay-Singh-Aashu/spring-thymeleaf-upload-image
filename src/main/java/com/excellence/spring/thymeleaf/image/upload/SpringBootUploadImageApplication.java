a2d978f6139b052d2757bbde74ba1e613ef9fab8
Vijay Singh <vijaysinghaashu@gmail.com>
2023-10-10 18:52:41 +0530

    upload-and-download-image-successfully

--- src/main/java/com/excellence/spring/thymeleaf/image/upload/SpringBootUploadImageApplication.java
+++ src/main/java/com/excellence/spring/thymeleaf/image/upload/SpringBootUploadImageApplication.java
@@ -1,18 +1,26 @@
 package com.excellence.spring.thymeleaf.image.upload;
 
+import javax.annotation.Resource;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.excellence.spring.thymeleaf.image.upload.service.FilesStorageService;
+
 @SpringBootApplication
 public class SpringBootUploadImageApplication implements CommandLineRunner {
 
+	@Resource
+	FilesStorageService storageService;
 
-  public static void main(String[] args) {
-    SpringApplication.run(SpringBootUploadImageApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUploadImageApplication.class, args);
+	}
 
-  @Override
-  public void run(String... arg) throws Exception {
-  }
+	@Override
+	public void run(String... arg) throws Exception {
+//	    storageService.deleteAll();
+		storageService.init();
+	}
 }
